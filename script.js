1bd3fa5f9c9cdd7096b480a51f95d81304127cc5
Yadin Nou <nouyadin@gmail.com>
2026-07-13 16:17:17 +1000

    update contact list functionalities

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const slide = document.getElementById("slide");
 const mobileLayout = document.getElementById("main");
 const contactList = document.getElementById("contactlist");
+const list = document.getElementById("list");
+
 const slideUnlock = () => {
   slide.remove(0);
-  mobileLayout.classList.add("active");
-  contactList.classList.remove("non-display");
+  //   slide.setAttribute("hidden", true);
+  mobileLayout.classList.add("contact");
+  contactList.removeAttribute("hidden");
 };
 
 const showContact = () => {
-  alert("show content");
+  //alert("show content");
+  mobileLayout.classList.remove("contact");
+  contactList.setAttribute("hidden", true);
+  list.removeAttribute("hidden");
 };
